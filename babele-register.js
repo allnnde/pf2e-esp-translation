e6f79052996d679b57404d0ebeaa98c52ba15bd0
Agustin <agustin_allende@hotmail.com>
2024-02-13 14:58:18 +0100

    fix: test

---
+++ babele-register.js
@@ -0,0 +1,121 @@
+// Prevent errors due to data structure changes - thanks to n1xx1 from the italian localization for the coding
+function removeMismatchingTypes(fallback, other = {}) {
+    for (let k of Object.keys(other)) {
+        const replacement = other[k];
+        const replacementType = getType(replacement);
+
+        if (!fallback.hasOwnProperty(k)) {
+            delete other[k];
+            continue;
+        }
+
+        const original = fallback[k];
+        const originalType = getType(original);
+
+        if (replacementType === "Object" && originalType === "Object") {
+            removeMismatchingTypes(original, replacement);
+            continue;
+        }
+
+        if (originalType !== "undefined" && replacementType !== originalType) {
+            delete other[k];
+        }
+    }
+
+    return fallback;
+}
+
+Hooks.once("init", () => {
+    if (typeof Babele !== "undefined") {
+        game.settings.register("lang-es-pf2e", "dual-language-names", {
+            name: "Nombres en Español e inglés",
+            hint: "Además del nombre aleman, también se utiliza el inglés.",
+            scope: "world",
+            type: Boolean,
+            default: false,
+            config: true,
+            onChange: foundry.utils.debounce(() => {
+                window.location.reload();
+            }, 100),
+        });
+
+        Babele.get().register({
+            module: "lang-es-pf2e",
+            lang: "es",
+            dir: "translation/es/compendium",
+        });
+/*
+        Babele.get().registerConverters({
+            normalizeName: (_data, translation) => {
+                return game.langEsPf2e.normalizeName(translation);
+            },
+            translateAdventureActorItems: (data, translation) => {
+                return game.langEsPf2e.translateActorItems(data, translation, false);
+            },
+            translateActorDescription: (data, translation) => {
+                return game.langEsPf2e.translateActorDescription(data, translation);
+            },
+            translateActorItems: (data, translation) => {
+                return game.langEsPf2e.translateActorItems(data, translation);
+            },
+            translateAdventureJournals: (data, translation) => {
+                return game.langEsPf2e.translateAdventureJournals(data, translation);
+            },
+            translateAdventureJournalPages: (data, translation) => {
+                return game.langEsPf2e.translateAdventureJournalPages(data, translation);
+            },
+            translateDualLanguage: (data, translation) => {
+                return game.langEsPf2e.translateDualLanguage(data, translation);
+            },
+            translateDuration: (data) => {
+                return game.langEsPf2e.translateValue("duration", data);
+            },
+            translateHeightening: (data, translation) => {
+                return game.langEsPf2e.dynamicObjectListMerge(
+                    data,
+                    translation,
+                    game.langEsPf2e.getMapping("heightening", true)
+                );
+            },
+            translateRange: (data) => {
+                return game.langEsPf2e.translateValue("range", data);
+            },
+            translateRules: (data, translation) => {
+                return game.langEsPf2e.translateRules(data, translation);
+            },
+            translateSource: (data) => {
+                return game.langEsPf2e.translateValue("source", data);
+            },
+            translateSpellVariant: (data, translation) => {
+                return game.langEsPf2e.dynamicObjectListMerge(
+                    data,
+                    translation,
+                    game.langEsPf2e.getMapping("item", true)
+                );
+            },
+            translateTiles: (data, translation) => {
+                return game.langEsPf2e.dynamicArrayMerge(data, translation, game.langEsPf2e.getMapping("tile", true));
+            },
+            translateTime: (data) => {
+                return game.langEsPf2e.translateValue("time", data);
+            },
+            translateTokenName: (data, translation, _dataObject, _translatedCompendium, translationObject) => {
+                return game.langEsPf2e.translateTokenName(data, translation, translationObject);
+            },
+            updateActorImage: (data, _translations, dataObject, translatedCompendium) => {
+                return game.langEsPf2e.updateImage("portrait", data, dataObject, translatedCompendium);
+            },
+            updateTokenImage: (data, _translations, dataObject, translatedCompendium) => {
+                return game.langEsPf2e.updateImage("token", data, dataObject, translatedCompendium);
+            },
+        });
+        */
+    }
+});
+
+Hooks.once("i18nInit", () => {
+    if (game.i18n.lang === "es") {
+        const fallback = game.i18n._fallback;
+        removeMismatchingTypes(fallback, game.i18n.translations);
+    }
+});
